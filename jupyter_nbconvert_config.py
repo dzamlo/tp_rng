e3ba21a87a853bade9806146653541a87c2c90ce
Loïc Damien <loic.damien@dzamlo.ch>
2016-05-08 20:09:17 +0200

    Add codefolding to the preprocessors

--- jupyter_nbconvert_config.py
+++ jupyter_nbconvert_config.py
@@ -2,5 +2,5 @@ c = get_config()
 c.NbConvertApp.notebooks = ['*.ipynb']
 c.NbConvertApp.export_format = 'pdf'
 c.Exporter.template_file = 'template'
-c.Exporter.preprocessors = ['pre_pymarkdown.PyMarkdownPreprocessor']
+c.Exporter.preprocessors = ['pre_codefolding.CodeFoldingPreprocessor', 'pre_pymarkdown.PyMarkdownPreprocessor']
 c.PDFExporter.latex_command = ['xelatex', '{filename}']
